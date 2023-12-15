dc3fb813a8712e6883de941010fd6a5bf55c22e3
Harry <hayerdevops@gmail.com>
2023-12-14 21:30:32 -0700

    added a javascript qr library, added logic to generate the qr and send as res to frontend

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -8,7 +8,7 @@ exports.generateQRCode = async (req, res) => {
         //begin generation
         const qrCodeImage = qrGenerator(data); //stub for generator
 
-        res.status(201).json({ message: 'QR Code generated!', qrCodeImage });
+        res.status(201).json({ message: 'QR Code generated!', qrCodeImage }); //send src to frontend as res
     } catch (error) {
         res.status(500).json({ message: 'Error generating QR code', error: error.message });
     }
--- util/qrGenerator.js
+++ util/qrGenerator.js
@@ -1,8 +1,14 @@
-//const qrCodeGeneratorLibrary = require('');
+const qrCodeGeneratorLibrary = require('qrcode');
 
-const qrGenerator = (data) => {
-
-    //return qrCodeGeneratorLibrary.generate(data, { type: 'base64' });
+const qrGenerator = async (data) => {
+    try {
+        //data is the URL from the frontend
+        const qrCodeDataURL = await qrCodeGeneratorLibrary.toDataURL(data);
+        return qrCodeDataURL; //this is the src for the frontend image tag
+    } catch (error) {
+        console.error('Error generating QR code:', error);
+        throw error;
+    }
 };
 
 module.exports = qrGenerator;
