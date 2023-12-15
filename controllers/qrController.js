f61b4c6393aec8b9bcdb2353b75bf5483effbe1f
Harry <hayerdevops@gmail.com>
2023-12-15 04:04:26 -0700

    modify controller to store qrcode/data pair in db for retrieval later

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -6,9 +6,17 @@ exports.generateQRCode = async (req, res) => {
 
     try {
         //begin generation
-        const qrCodeImage = qrGenerator(data); //stub for generator
+        const qrCodeImage = await qrGenerator(data);
 
-        res.status(201).json({ message: 'QR Code generated!', qrCodeImage }); //send src to frontend as res
+        //create a new QR code record in our db
+        const newQRCode = new qrModel({
+            data: data, //url
+            image: qrCodeImage //qr code
+        });
+
+        await newQRCode.save();
+
+        res.status(201).json({ message: 'QR Code generated!', qrCodeImage }); // Send src to frontend as res
     } catch (error) {
         res.status(500).json({ message: 'Error generating QR code', error: error.message });
     }
